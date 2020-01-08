2374fb578381478aa8e1ba096b3cd20e6ef2eee3
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-08 04:56:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+require 'pry'
+def map(source_array)
+  result = []
+  counter = 0 
+  while counter < source_array.size 
+    result << yield(source_array[counter])
+    counter += 1
+  end
+  result
+end
+
+def reduce(source_array, starting_value = nil)
+  if starting_value
+    sum = starting_value
+    index = 0
+  else
+    sum = source_array[0]
+    index = 1
+  end
+  while index < source_array.size
+    sum = yield(sum, source_array[index])
+    index += 1
+  end
+  sum
+end

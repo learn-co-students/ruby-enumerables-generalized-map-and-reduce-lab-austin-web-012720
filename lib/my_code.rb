519737d2ea980edcbcc2ce54cecb2b5da2523c54
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 23:09:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length do 
+   new_array << yield(array[i])
+   i += 1
+  end 
+  new_array
+end
+
+def reduce(array, sv=nil)
+  
+  if sv
+    total = sv
+    i = 0
+  else 
+    total = array[0]
+    i = 1
+  end
+  
+  while i < array.length do 
+  total = yield(total, array[i])
+  i += 1
+  end 
+  total
+end 

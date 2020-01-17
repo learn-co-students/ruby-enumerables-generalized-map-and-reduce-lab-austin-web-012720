ce943b47c045737d10cdda865a58a781250de98b
Jacques Hebert <jacques.henri.hebert@gmail.com>
2020-01-17 03:31:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(a)
+  n = []
+    for i in 0...a.length 
+       n << yield(a[i])
+    end
+  return n 
+end
+
+def reduce(a, sp=nil)
+    if sp
+      t = sp
+      start = 0
+    else
+      t = a[0]
+      start = 1
+    end
+    for i in start...a.length 
+        t = yield(t, a[i])
+    end
+    return t
+end

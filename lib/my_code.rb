02d087a28c0a2b22289ee28ca5fa578dc9c8e1b2
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 20:18:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+# Your Code Her
+def map(array)
+  arrays = []
+  v = 0
+  while v < array.length
+    arrays.push(yield(array[v]))
+  v += 1
+  end
+  arrays
+end
+
+def reduce(array,starting_value=nil)
+  if starting_value
+    sum = starting_value
+    i = 0
+  else
+    sum = array [0]
+    i = 1
+  end
+  
+  while i < array.length
+    sum = yield(sum,array[i])
+    i += 1
+  end
+  sum
+end

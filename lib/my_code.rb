63fd5b16350f0c05f28aa5c8e2c7b66e109b30ab
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-21 02:00:53 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(array)
+  new_arr = []
+  
+  for i in 0...array.length
+    new_arr.push(yield(array[i]))
+  end
+  
+  new_arr
+end
+
+def reduce(array, start_val=nil)
+  
+  if start_val
+    sum = start_val
+    for i in 0...array.length
+      sum = yield(sum, array[i])
+    end 
+  else 
+    sum = array[0]
+    for i in 1...array.length
+      sum = yield(sum, array[i])
+    end
+  end
+  
+  
+  
+  return sum
+end
